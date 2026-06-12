292cb0e925b399b427175a9ebddd790106492142
Septian Eka Cahya Tamonob <aan250906@gmail.com>
2026-06-12 16:55:10 +0700

    feat: membuat class KmsController dan Main launcher

--- src/main/java/kms_java/app/Main.java
+++ src/main/java/kms_java/app/Main.java
@@ -1,4 +1,10 @@
 package kms_java.app;
 
+import kms_java.controller.KmsController;
+
 public class Main {
+    public static void main(String[] args) {
+        KmsController controller = new KmsController();
+        controller.mulaiAplikasi();
+    }
 }
