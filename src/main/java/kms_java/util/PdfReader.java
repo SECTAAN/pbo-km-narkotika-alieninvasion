031380119f2c6da82b9da9804dcbdf1c75bef18e
Septian Eka Cahya Tamonob <aan250906@gmail.com>
2026-06-13 19:59:21 +0700

    feat: menambahkan library Apache PDFBox dan inisialisasi class PdfReader

--- src/main/java/kms_java/util/PdfReader.java
+++ src/main/java/kms_java/util/PdfReader.java
@@ -1,4 +1,36 @@
 package kms_java.util;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import java.io.File;
+import java.io.IOException;
+
 public class PdfReader {
+
+    public String bacaTeksDariPdf(String lokasiFile) {
+        String teksHasil = "";
+        PDDocument dokumen = null;
+
+        try {
+            File filePdf = new File(lokasiFile);
+            dokumen = PDDocument.load(filePdf);
+
+            PDFTextStripper penyedotTeks = new PDFTextStripper();
+            teksHasil = penyedotTeks.getText(dokumen);
+
+        } catch (IOException e) {
+            System.out.println("[ERROR] Gagal membaca file PDF di: " + lokasiFile);
+            System.out.println("Pesan Error: " + e.getMessage());
+        } finally {
+            try {
+                if (dokumen != null) {
+                    dokumen.close();
+                }
+            } catch (IOException ex) {
+                System.out.println("[ERROR] Gagal menutup file PDF.");
+            }
+        }
+
+        return teksHasil;
+    }
 }
